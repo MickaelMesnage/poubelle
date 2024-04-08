239df3aab5057ab1a3a6f36a730498677bfdcfe4
Mickael <mickael.mesnage@lonestone.io>
2024-04-08 15:20:06 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <Link href="https://dev.mimbi.io/ref">Test</Link>
+      <Link href="https://app.fullmotiv.com/test">Test</Link>
     </main>
   );
 }
